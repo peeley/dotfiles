84cda631eaa624689999fc157ef98a9c5ae2f532
Noah Snelson <noah.snelson@protonmail.com>
2020-01-29 11:17:18 -0800

    Wrote script to automatically install all vim plugins

---
+++ .vim/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# script to install all vim plugins into ~/.vim/bundle
+cd bundle/
+
+# NERDTree - file explorer
+git clone https://github.com/preservim/nerdtree.git
+# Syntastic - syntax highlighting & syntax checking
+git clone https://github.com/vim-syntastic/syntastic.git
+# YouCompleteMe - auto-complete
+git clone https://github.com/ycm-core/YouCompleteMe.git
+# rust.vim - rust language Syntastic integration, syntax highlighting, etc.
+git clone https://github.com/rust-lang/rust.vim.git
+# vimtex - compile & view LaTeX automatically
+git clone https://github.com/lervag/vimtex.git
+
+# additional YCM language support for Go, JS, Rust, etc. need to be installed
+cd YouCompleteMe
+python3 install.py --all
+
+cd ../
+echo "\n Done! Enjoy Vim :)"
