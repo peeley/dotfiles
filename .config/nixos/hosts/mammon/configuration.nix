a1e13b908a4a002e0ea7c35d3d12d76777cd13d6
Noah Snelson <noah.snelson@protonmail.com>
2026-03-08 11:47:01 -0700

    Add udev rule for Logitech G29 wheel

--- .config/nixos/hosts/mammon/configuration.nix
+++ .config/nixos/hosts/mammon/configuration.nix
@@ -87,6 +87,11 @@
   programs.steam.enable = true;
   hardware.steam-hardware.enable = true;
 
+  # hardware.new-lg4ff.enable = true;
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c261", RUN+="${pkgs.usb-modeswitch}/bin/usb_modeswitch -v 046d -p c261 -m 01 -r 01 -C 03 -M '0f00010142'"
+  '';
+
   programs.git = {
     enable = true;
   };
